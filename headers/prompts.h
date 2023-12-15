f44732c7ece2857b24a19c6ec7593981c2ccecba
HallRules <133172738+HallRules@users.noreply.github.com>
2023-12-14 20:12:54 -0500

    old code 2

--- headers/prompts.h
+++
@@ -1,7 +0,0 @@
-#include "classes.h"
-#include <vector>
-using namespace std;
-void addClass_prompt(vector<GPA_Class>& classes);
-void removeClass_prompt(vector<GPA_Class>& classes);
-void editClass_prompt(vector<GPA_Class>& classes);
-void loadClasses_prompt(vector<GPA_Class>& classes);
