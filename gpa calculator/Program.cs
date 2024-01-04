5878a70fe5842c43c8eb4f9dea32489e24e471d2
HallRules <133172738+HallRules@users.noreply.github.com>
2024-01-03 22:25:44 -0500

    azure db commit all

--- gpa calculator/Program.cs
+++ gpa calculator/Program.cs
@@ -11,7 +11,7 @@ namespace gpa_calculator
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("gpa_calculator") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
